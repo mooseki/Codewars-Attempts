53d39058395c49dc968fb8f9e90eddf27afaded3
Antony Ekirapa <85803523+NueMe@users.noreply.github.com>
2022-06-08 17:22:47 +0300

    6 kyu Return the missing letter in the array

--- Find-the-missing-letter.js
+++ Find-the-missing-letter.js
@@ -1,6 +1,6 @@
 // Instructions
 
-// #Find the missing letter
+// #Find the missing letter 
 
 // Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
 
