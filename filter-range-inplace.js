583da35444bee29ff83f909f06097b5caf24b79c
Antony Ekirapa <85803523+NueMe@users.noreply.github.com>
2022-12-30 05:09:33 +0300

    Write function to modify array

---
+++ filter-range-inplace.js
@@ -0,0 +1,42 @@
+// Filter range "in place"
+
+// Write a function filterRangeInPlace(arr, a, b) that gets an array arr and removes from it all values except those that are between a and b. The test is: a ≤ arr[i] ≤ b.
+
+// The function should only modify the array. It should not return anything.
+
+// For instance:
+
+// let arr = [5, 3, 8, 1];
+
+// filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
+
+// alert( arr ); // [3, 1]
+
+// Attempt:
+function filterRange(arr, a, b) {
+  let newArr = arr.filter((item) => item >= a && item <= b);
+
+  arr = newArr;
+  console.log(arr);
+}
+
+filterRange([5, 3, 8, 1], 1, 4);
+
+// Optimized solution:
+function filterRangeInPlace(arr, a, b) {
+  for (let i = 0; i < arr.length; i++) {
+    let val = arr[i];
+
+    // remove if outside of the interval
+    if (val < a || val > b) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+}
+
+let arr = [5, 3, 8, 1];
+
+filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
+
+alert(arr); // [3, 1]
