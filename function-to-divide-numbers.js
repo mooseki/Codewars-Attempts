6bde33dd9bb9cdb45218080494bfcff9bf888283
Antony Ekirapa <a.m.ekirapa@gmail.com>
2023-01-25 02:19:42 +0300

    function that takes in three numbers

---
+++ function-to-divide-numbers.js
@@ -0,0 +1,9 @@
+//---Medium
+//create a function that takes in three numbers. Add the first two numbers and return the remainder of dividing the sum of the first two
+//numbers by the third number
+
+function sumAndDivide(n1, n2, n3) {
+  return (n1 + n2) % n3;
+}
+
+// const sumAndDivide(2,3,5)
